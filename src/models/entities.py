cf4b6be260cd16279151eee9b4f988672b20ea7e
KhDan007 <kh.dan007@gmail.com>
2025-12-24 20:42:12 +0500

    change entities for a major feature

--- src/models/entities.py
+++ src/models/entities.py
@@ -1,11 +1,19 @@
 from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Group:
+    id: int
+    name: str  # e.g., "CS-SL-26-1"
 
 @dataclass
 class Student:
     id: int
     name: str
     roll_number: str
-    encoding_path: str
+    encoding_path: Optional[str]
+    group_id: int
+    group_name: str = "" # Helper for display
 
 @dataclass
 class Course:
@@ -17,6 +25,9 @@ class Course:
 class TimetableSlot:
     id: int
     course_id: int
-    day_of_week: int # 0=Monday, 6=Sunday
-    start_time: str  # Format "HH:MM"
-    end_time: str    # Format "HH:MM"
+    group_id: int
+    day_of_week: int
+    start_time: str
+    end_time: str
+    group_name: str = ""  # Helper
+    course_name: str = "" # Helper
